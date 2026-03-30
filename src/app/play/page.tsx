c09728286e2fafee74b87c0de30d13d0b2161598
richardkurspe <richard.d.kurspe@gmail.com>
2026-03-31 03:23:50 +0800

    Fix conditional rendering for external transcode button

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -8423,7 +8423,7 @@ function PlayPageClient() {
                           </span>
                         </button>
 
-                        {showExternalTranscodeButton && (
+                        {true && (
                           <button
                             onClick={async (e) => {
                               e.preventDefault();
